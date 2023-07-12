a9f06e1f43c9008adb3a4616fd516ca7aa35676a
Braulio Ricardo Lopez Chavez <braulio20001@gmail.com>
2023-07-12 09:42:49 -0700

    Codificacion de la tabla Bd

---
+++ app/src/main/java/com/example/preexamen9_3c2/modelo/DefineTable.java
@@ -0,0 +1,22 @@
+package com.example.preexamen9_3c2.modelo;
+
+import android.provider.BaseColumns;
+
+public class DefineTable implements BaseColumns {
+    public DefineTable(){}
+
+    public static abstract class Usuarios {
+        public static final String TABLE_NAME = "usuarios";
+        public static final String COLUMN_NAME_ID = "id";
+        public  static final String COLUMN_NAME_USUARIO = "usuario";
+        public static final String COLUMN_NAME_CORREO = "correo";
+        public static final String COLUMN_NAME_CONTRA = "contra";
+
+        public static String[] REGISTRO = new String[]{
+                Usuarios.COLUMN_NAME_ID,
+                Usuarios.COLUMN_NAME_USUARIO,
+                Usuarios.COLUMN_NAME_CORREO,
+                Usuarios.COLUMN_NAME_CONTRA
+        };
+    }
+}
